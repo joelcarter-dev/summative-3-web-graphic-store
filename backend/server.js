b72594ff635551a74bcca0b8e075f63691dd4176
Joel <joel.innes@gmail.com>
2020-04-12 03:38:57 +1300

    added more dependencys

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,71 @@
-const express = require('express')
-const cors = require('cors')
-const BP = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+//Setup s2: Import mongoose
+const mongoose = require("mongoose");
 
-const app = express()
+const dotenv = require("dotenv");
+dotenv.config();
 
-//modals
-// require('./modals/Article')
+var isProduction = process.env.NODE_ENV === 'production';
 
-//routs 
-app.use(BP.urlencoded({extended: false}))
-app.use(BP.json())
-app.use(cors())
-app.use(require('./routs'))
+// Create global Express object
+var app = express();
 
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Server Running at " + server.address().port)
+//allow cross origin requests 
+app.use(cors());
 
-})
+//Setup s1: Use body-parser middleware so we can access req.body in our route handlers (see routes/v1/articles.js)
+//Normal express config defaults
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// //Setup s3: Require all the models
+// require("./models/Article");
+// //Setup s5: Require the User model
+// require("./models/User");
+
+// //Setup s1: Use the routes
+// app.use(require('./routes'));
+
+//Setup s2: Connect the database
+mongoose.connect(process.env.MONGODB_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+mongoose.connection.on("open", function (ref) {
+  console.log("✔ MongoDB connected");
+});
+
+
+/// Error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+// finally, let's start our server...
+var server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port);
+});
