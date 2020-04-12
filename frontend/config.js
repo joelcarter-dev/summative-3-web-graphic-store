35f8d95bc8f29de4c11b513338dd21ab02f8d1ad
Joel <joel.innes@gmail.com>
2020-04-13 03:50:45 +1300

    added base vue routing

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    apiUrl: 'http://localhost:3000/v1/'
+}
+
+
---
+++ frontend/vue-config.js
@@ -0,0 +1,23 @@
+const GoogleFontsPlugin = require('google-fonts-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+    plugins: [
+      new GoogleFontsPlugin({
+        fonts: [
+          // add your fonts, like
+          // { family: "Source Sans Pro" },
+        
+        ]
+      })
+    ]
+};
