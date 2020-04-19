85034994d594703dca8827163365595b588f6b3d
Chris Lewis <chrislewis@Chriss-MacBook-Pro.local>
2020-04-19 16:45:05 +1200

    nav and info

--- frontend/src/components/shared-components/Navbar.vue
+++ frontend/src/components/shared-components/Navbar.vue
@@ -1,34 +1,50 @@
 <template>
   <div class="header">
-    <div class="left">
-      <img src="../../../img/ARTmarket.png" alt="ARTmarket" />
-    </div>
-    <div class="right">
-      <ul>
-        <li>Home</li>
-        <li>List an item</li>
-        <li>{{ login }}</li>
-      </ul>
-    </div>
+    <img class="logo" src="../../../img/ARTmarket.png" alt="ARTmarket" />
+    <ul>
+      <li>Home</li>
+      <!-- <li>{{ login }}</li> -->
+      <li>List an item</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import GetIsLoggedIn from "../../services/auth-service";
 export default {
   name: "Navbar",
   data() {
     return {
       nav: {
-        login: true
-      }
-    }
-  }
-}
+        login: true,
+      },
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
 @import "../../lib/vars"
+
 .header
-  height: 2rem
+  display: flex
+  justify-content: space-between
+  align-items: center
+  padding: 1rem 2rem
   background-color: #1A1A1A
+  color: white
+
+ul
+  display: flex
+  justify-content: flex-end
+  // not working, still aligns to the left of the container
+  padding: 0
+
+li
+  text-align: right
+  margin: 0rem 0rem 0rem 1rem
+  list-style-type: none
+
+.logo
+  height: 2rem
 </style>
--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -1,17 +1,19 @@
 <template>
-	<div class="">
-		<Navbar />
-		<GetItems :showDetailed="false"/>
-	</div>
+  <div class="">
+    <Navbar />
+    <InfoSection />
+    <GetItems :showDetailed="false" />
+  </div>
 </template>
 
 <script>
-	import GetItems from '../components/GetItems'
-	import Navbar from '../components/shared-components/Navbar'
-	export default {
-		name: "LandingPage",
-		components: {GetItems, Navbar}
-	}
+import GetItems from "../components/GetItems";
+import Navbar from "../components/shared-components/Navbar";
+import InfoSection from "../components/Info";
+export default {
+  name: "LandingPage",
+  components: { GetItems, Navbar },
+};
 </script>
 
 <style lang="sass" scoped></style>
