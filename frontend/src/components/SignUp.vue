f15f25efe246550453fca1f36411f13085b99b09
Chris Lewis <chrislewis@Chriss-MacBook-Pro.local>
2020-04-22 15:01:15 +1200

    updated user schema, created signup.vue

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="modal">
+    <h1>Sign up</h1>
+    <form class="login-form">
+      <h2>Name</h2>
+      <input type="text" />
+      <h2>Username</h2>
+      <input type="text" />
+      <h2>Email address</h2>
+      <input type="text" />
+    </form>
+    <Btn @click: />
+  </div>
+</template>
+
+<script>
+import axios from axios
+import * as config from "../../config"
+export default {
+    props: [],
+    name: "SignUp",
+    data: function () {
+        return {
+            user: {
+                name: "",
+                userName: "",
+                email: ""
+            }
+        }
+    },
+    methods: {
+        checkForm: function(event) {
+			event.preventDefault()
+				this.errors = []
+				if (this.ohNoHoney) {
+					this.errors.push("Gotcha")
+				}
+				if (!this.user.email) {
+					this.errors.push("Email Address required")
+				}
+				if (!this.errors.length) {
+					this.loginUser()
+				}
+		    },
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+</style>
